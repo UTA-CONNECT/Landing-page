23e2a832c7c1bd58e7180faac8f5557e4dbffdb5
Kim HyunWoo <stories2@naver.com>
2021-06-17 23:46:04 +0900

    firebase 스크립트 추가

---
+++ public/src/firebase-script.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDO8ktHyj4xjeR1Pl062QkNYZ7exCCCIFE",
+    authDomain: "uta-conne.firebaseapp.com",
+    projectId: "uta-conne",
+    storageBucket: "uta-conne.appspot.com",
+    messagingSenderId: "161018803676",
+    appId: "1:161018803676:web:7e086c9af1e90703bccb19",
+    measurementId: "G-KCS7BSHSED"
+};
+
+firebase.initializeApp(firebaseConfig);
